e37ebcc9df66f6346d6b7e910a2eee4a16e05ec8
Mark <75143899+qlemon-56@users.noreply.github.com>
2025-08-15 09:22:16 +0100

    update 1.0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace std;
+using namespace cv;
 
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
 
+    std::string inputPath = samples::findFile("JLR Logo Header.png");
+    Mat img = imread(inputPath, IMREAD_COLOR);
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image" << std::endl;
+        return 1;
+    } 
+
+    imshow("Display window", img);
+
+    int k = waitKey(0);
+
+    if (k == 's')
+    {
+        imwrite("output.jpg", img);
+    }
+    
+
+    return 0;
+}   
